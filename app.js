172c0c61358e6483ef69e0755cb8597f85a00532
Leonardo Grigoletto <grigolettoleonardo@gmail.com>
2024-09-04 19:06:41 -0300

    Final do projeto V-00

---
+++ app.js
@@ -0,0 +1,98 @@
+//Pegando valores
+function pegandoParametros(){
+    let idS = ["quantidade","de","ate"];
+    let valoresInput=[];
+    let coletaInput;
+    
+    for(i=0;i<idS.length;i++){
+        coletaInput = parseInt(document.getElementById(idS[i]).value);
+        valoresInput.push(coletaInput);
+    }
+
+    return valoresInput;
+}
+
+function verificacaoInput(listaEntrada){
+    let valoresEntrada = listaEntrada;
+
+    if (valoresEntrada[0]>0){
+
+        if(valoresEntrada[1] > valoresEntrada[2]){
+            alert(`O primeiro valor ${valoresEntrada[1]} tem que ser menor que o segundo valor ${valoresEntrada[2]}`);
+        } else{
+            mostraResultado();
+        }
+
+    } else{
+
+        alert("Valor de entrada tem que ser maior que 0!!")
+        
+    }
+}
+
+
+function gerandoNumeroSecreto(){
+
+    let valores = pegandoParametros();
+    let quantidade = valores[0];
+    let menorValor = valores[1];
+    let maiorValor = valores[2];
+    let resultados = [];
+    let numeroAleatorio;
+    let somador = 0;
+
+    //Loop para não repetir valores
+    while(somador<=quantidade){
+
+        numeroAleatorio = Math.floor(Math.random() * (maiorValor - menorValor + 1)) + menorValor;
+
+        if(resultados.includes(numeroAleatorio)){
+            continue;
+        } else{
+            resultados.push(numeroAleatorio);            
+        }
+        somador ++;
+    }
+    return resultados;
+}
+
+function numerosCrescentes(){
+
+    let valoresAleatorios = gerandoNumeroSecreto();
+    let valoresCrescente = [];
+    let menorValor;
+    let indiceMenorValor;
+
+    console.log(valoresAleatorios);
+
+    for(i=valoresAleatorios.length - 1;i>=0;i--){
+        menorValor = Math.min(...valoresAleatorios);
+        indiceMenorValor = valoresAleatorios.indexOf(menorValor);
+        valoresCrescente.push(menorValor);
+        valoresAleatorios.splice(indiceMenorValor,1);
+    }   
+    return valoresCrescente;
+}
+
+
+function mostraResultado(){
+
+    let valoresFinais = numerosCrescentes();
+    console.log(valoresFinais);
+    const resultadoDiv = document.getElementById("resultado");
+    const resultadoLabel = resultadoDiv.firstChild;
+    resultadoLabel.textContent =`Os números aleatórios gerados são: ${valoresFinais}`;
+
+}
+
+function reiniciar(){
+    
+}
+
+
+
+function sortear(){
+    let valoresUsuario = pegandoParametros();
+    verificacaoInput(valoresUsuario);
+
+}
